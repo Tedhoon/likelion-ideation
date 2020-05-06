203c12a727bf4df5bc323d8aaee20b402219771b
Tedhoon <gt0305@likelion.org>
2020-05-07 04:46:05 +0900

    [add] 자세히 보기 페이지 animation 구성, 광고 추가

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const printer = document.querySelector('.printer'),
           printBtn = document.querySelector('.print-btn'),
           hashTag = document.querySelector('.hash-tag'),
-          lightbox = document.querySelector('.lightbox'),
-          lightboxImage = document.querySelector('.lightbox-image'),
-          resetBtn = document.querySelector('.reset_btn');
+          advertise = document.querySelector('.advertise'),
+          printTray = document.querySelector('.printer-tray');
 
     let printing = false;
 
@@ -13,13 +12,15 @@ document.addEventListener("DOMContentLoaded", () => {
         if (!printing) printer.classList.add('printing');
     });
 
-    hashTag.addEventListener('click', () => lightbox.classList.add('show'));
-
-    lightbox.addEventListener('click', () => lightbox.classList.remove('show'));
-
-    resetBtn.addEventListener('click',(e) => {
-        // lightbox.classList.remove('show');
-        e.stopPropagation()
-        location.reload()
-    })
+    hashTag.addEventListener('click', () => {
+        if(printTray.className === 'printer-tray'){
+            printTray.classList.add('show-up')
+            advertise.classList.add('adshow')
+        }
+        else{
+            printTray.classList.remove('show-up')
+            advertise.classList.remove('adshow')
+        }
+        
+    });
 });
